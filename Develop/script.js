4868fc2f1ca7c6bc4d719c81ad87136abecd57dc
unknown <el_word9@yahoo.com>
2023-10-16 02:43:14 -0500

    Finished adding coordinates , long and lad

--- Develop/script.js
+++ Develop/script.js
@@ -18,12 +18,12 @@ fetch(apiUrl)
     console.error(error);
   });
   const cities = [
-    { name: 'Atlanta', latitude: 40.7128, longitude: -74.0060 },
-    { name: 'Denver', latitude: 34.0522, longitude: -118.2437 },
-    { name: 'Seattle', latitude: 51.5074, longitude: -0.1278 },
-    { name: 'San Francisco', latitude: 40.7128, longitude: -74.0060 },
-    { name: 'Orlando', latitude: 34.0522, longitude: -118.2437 },
-    { name: 'New York', latitude: 51.5074, longitude: -0.1278 },
-    { name: 'Chicago', latitude: 40.7128, longitude: -74.0060 },
-    { name: 'Austin', latitude: 40.7128, longitude: -74.0060 },
+    { name: 'Atlanta', latitude: 33.74470758742555, longitude:  -84.40401172646862 },
+    { name: 'Denver', latitude: 39.746978665122896, longitude:  -104.982891722931 },
+    { name: 'Seattle', latitude: 47.608013, longitude: -122.335167 },
+    { name: 'San Francisco', latitude: 37.773972, longitude: -122.431297 },
+    { name: 'Orlando', latitude: 28.538336, longitude:  -81.379234 },
+    { name: 'New York', latitude: 40.730610, longitude:  -73.935242 },
+    { name: 'Chicago', latitude: 41.881832, longitude:  -87.623177 },
+    { name: 'Austin', latitude: 30.266666, longitude:  -97.733330 },
   ];
